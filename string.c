68c90b3ed0784f06168cc939318ecaa98bf93a31
Sajal <sajal.kumar2003@gmail.com>
2024-01-12 00:15:38 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,53 +1,50 @@
 #include <stdio.h>
 
-char str[100], pat[100], rep[100];
-int pat_len;
+void read_strings(char str[], char pat[], char rep[]) {
+    printf("Enter the main string: ");
+    gets(str);
 
-void read_strings() {
-    int i;
-    printf("\nEnter the main string: ");
-    fgets(str, sizeof(str), stdin);
-    printf("\nEnter the pattern string: ");
-    fgets(pat, sizeof(pat), stdin);
-    printf("\nEnter the replace string: ");
-    fgets(rep, sizeof(rep), stdin);
+    printf("Enter the pattern string: ");
+    gets(pat);
 
-    for (i = 0; pat[i] != '\0'; i++)
-        pat_len++;
+    printf("Enter the replace string: ");
+    gets(rep);
 }
 
-void pattern_match() {
-    int i, j, k, z = 0, count = 0;
+void pattern_replace(char str[], char pat[], char rep[]) {
+    int i, j, k, count = 0;
     char final_str[100];
 
     for (i = 0; str[i] != '\0'; i++) {
-        j = 0;
-        while ((str[i + j] == pat[j]) && (j < pat_len))
-            j++;
+        for (j = 0; pat[j] != '\0' && str[i + j] == pat[j]; j++);
 
         if (pat[j] == '\0') {
             count++;
-            for (k = 0; rep[k] != '\0'; k++, z++) {
-                final_str[z] = rep[k];
-            }
-            i = i + pat_len - 1;
+
+            for (k = 0; rep[k] != '\0'; k++)
+                final_str[i + k] = rep[k];
+
+            i += j - 1;
         } else {
-            final_str[z] = str[i];
-            z++;
+            final_str[i] = str[i];
         }
     }
 
-    if (count == 0)
+    final_str[i] = '\0';
+
+    if (count == 0) {
         printf("\nPattern doesn't exist in the main string!!!");
-    else {
-        final_str[z] = '\0';
+    } else {
         printf("\nThe number of occurrences of the pattern: %d", count);
         printf("\nThe main string after replacement is: %s\n", final_str);
     }
 }
 
 int main() {
-    read_strings();
-    pattern_match();
+    char str[100], pat[100], rep[100];
+
+    read_strings(str, pat, rep);
+    pattern_replace(str, pat, rep);
+
     return 0;
 }
